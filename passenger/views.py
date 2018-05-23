524062abea2cb33daf6bfff3f4a0a422a0a2cbff
christinekarimi <karimikim3@gmail.com>
2018-05-23 11:08:33 +0300

    Added readme

--- passenger/views.py
+++ passenger/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm,ProfileForm
 from .models import Passenger_profile
-# from drivers.models import Driver
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
