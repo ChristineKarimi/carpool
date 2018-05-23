acfd29ecc760cf08753aefb7f8585842ad7de268
christinekarimi <karimikim3@gmail.com>
2018-05-23 10:56:15 +0300

    Created a view function for the passenger.html template

--- passenger/views.py
+++ passenger/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm,ProfileForm
+from .models import Passenger_profile
+# from drivers.models import Driver
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+def rider(request):
+  user = User.objects.get(username = request.user.username)
+  profile = Rider_profile.objects.get(user =user)
+  return render(request, 'passengers/passenger.html')
