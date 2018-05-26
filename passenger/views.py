d6f587b97fe4dd79a2a2ed9b98d10676046c583f
christinekarimi <karimikim3@gmail.com>
2018-05-26 12:58:56 +0300

    Handled imports and created a passenger view function

--- passenger/views.py
+++ passenger/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm,ProfileForm
-from .models import Passenger_profile
+from .models import Rider_profile
+from drivers.models import Driver_profile,TripPlan,Booking
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 
 # Create your views here.
 def rider(request):
   user = User.objects.get(username = request.user.username)
   profile = Rider_profile.objects.get(user =user)
-  return render(request, 'passengers/passenger.html')
+  drivers = Driver_profile.objects.all()
+  return render(request, 'riders/rider.html', {"profile": profile, "drivers":drivers})
