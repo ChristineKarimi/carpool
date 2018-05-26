44409952464b1ee9bbaeb5e15cc4b43d8a7a3a82
christinekarimi <karimikim3@gmail.com>
2018-05-26 13:01:25 +0300

    Defined urlconf for the passenger app

--- passenger/views.py
+++ passenger/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm,ProfileForm
-from .models import Rider_profile
-from drivers.models import Driver_profile,TripPlan,Booking
+from .models import Passenger_profile
+# from drivers.models import Driver_profile,TripPlan,Booking
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 
 # Create your views here.
-def rider(request):
+def passenger(request):
   user = User.objects.get(username = request.user.username)
-  profile = Rider_profile.objects.get(user =user)
+  profile = Passenger_profile.objects.get(user =user)
   drivers = Driver_profile.objects.all()
-  return render(request, 'riders/rider.html', {"profile": profile, "drivers":drivers})
+  return render(request, 'passenger/passenger.html', {"profile": profile, "drivers":drivers})
