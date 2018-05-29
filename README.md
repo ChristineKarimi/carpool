4b3f0b5ee9ab14a44e6015f09efa26d530b65724
christinekarimi <karimikim3@gmail.com>
2018-05-29 10:55:50 +0300

    Added readme

--- README.md
+++ README.md
@@ -17,7 +17,20 @@ Virtual Env
 ```
 Pip
 ```
+**User Stories**
 
+    _As a User, I would like to:_
+    
+        <o> login onto the website, find a driver near me with space in 
+                his/her car going the same direction as me and book a space
+            
+        <o> setup a profile about me and my general location.
+        
+        <o> find a list of drivers near me.
+        
+        <o> view a map with the location of all pickup points.
+        
+        <o> review a driver and also be reviewed by the driver.
 
 
 
--- driver/tests.py
+++ driver/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile, Rider, Passenger
+
+from django.contrib.auth.models import User
+
+
+
+
+
+class ProfileTestClass(TestCase):
+
+    """test class for Profile model"""
+
+
+
+    def setUp(self):
+
+        self.user = User.objects.create_user("testuser", "karimikim3@gmail.com", "secret")
+
+
+
+        self.new_profile_test = Profile(
+
+            profile_photo='profiles/profile.jpg', profile_owner=self.user)
+
+
+
+    def test_instance_true(self):
+
+        self.new_profile_test.save()
+
+        self.assertTrue(isinstance(self.new_profile_test, Profile))
+
+
+
+    def test_save_profile_method_true(self):
+
+        self.new_profile_test.save_profile()
+
+        profiles = Profile.objects.all()
+
+        self.assertTrue(len(profiles) == 1)
+
+
+
+    def test_delete_profile_method_true(self):
+
+        self.new_profile_test.save_profile()
+
+        self.new_profile_test.delete_profile()
+
+        profiles = Profile.objects.all()
+
+        self.assertTrue(len(profiles) == 0)
+
